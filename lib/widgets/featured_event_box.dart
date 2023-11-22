e7dedc0a34b7df274a070670c222adff439845d1
Thato Lebethe <thatofreelance@gmail.com>
2023-11-22 18:34:12 +0200

    featured event

--- lib/widgets/featured_event_box.dart
+++ lib/widgets/featured_event_box.dart
@@ -1,4 +1,5 @@
 import 'package:community_hub/constants/colors.dart';
+import 'package:community_hub/screens/home/shared_home/event_details_screen.dart';
 import 'package:flutter/material.dart';
 
 class FeaturedBox extends StatelessWidget {
@@ -7,56 +8,66 @@ class FeaturedBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(
-        top: 20,
-        right: 20,
-      ),
-      width: 160,
-      height: 220,
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          fit: BoxFit.fill,
-          image: AssetImage(
-            boxData['image'],
-          ),
+    return InkWell(
+      onTap: () {
+        print('get more details');
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => ViewEventDetails()));
+      },
+      child: Container(
+        margin: const EdgeInsets.only(
+          top: 20,
+          right: 20,
         ),
-        color: greenColor,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Stack(
-              children: [
-                Container(
-                  color: darkColor.withOpacity(0.7),
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height * 0.125,
-                ),
-                Text(
-                  boxData['title'],
-                  style: const TextStyle(
-                    color: greenColor,
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
+        width: 160,
+        height: 220,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage(
+              boxData['image'],
+            ),
+          ),
+          color: greenColor,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: darkColor.withOpacity(0.6),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height * 0.125,
                   ),
-                ),
-                Positioned(
-                  bottom: 10,
-                  child: Text(
-                    boxData['establishment'],
+                  Text(
+                    boxData['title'],
                     style: const TextStyle(
-                      color: whiteColor,
-                      fontSize: 18,
+                      color: greenColor,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  Positioned(
+                    bottom: 10,
+                    child: Text(
+                      boxData['establishment'],
+                      style: const TextStyle(
+                        color: whiteColor,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
